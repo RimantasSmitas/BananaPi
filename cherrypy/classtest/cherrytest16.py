19d892260e59ef81b1684dac14dfe20c624ca525
Monika Styxová <monikastyxova@Monikas-MacBook-Air-2.local>
2017-11-03 10:49:22 +0100

    Fixed indentation

--- cherrypy/classtest/cherrytest16.py
+++ cherrypy/classtest/cherrytest16.py
@@ -1,8 +1,6 @@
 import cherrypy
 import RPi.GPIO as GPIO
 
-#This is a comment
-
 GPIO.cleanup()
 file = open('/home/pi/robocar/cherrypy/classtest/htmlfile3.html')
 index_html = file.read()
@@ -27,73 +25,72 @@ GPIO.setup(BIN1, GPIO.OUT)
 GPIO.setup(BIN2, GPIO.OUT)
 
 class motorController:
-	@cherrypy.expose
-	def SetMotorAforward(motorShouldRun):
-		if(motorShouldRun):
-			GPIO.output(AIN1, GPIO.HIGH)
-			GPIO.output(AIN2, GPIO.LOW)
-			GPIO.output(PWMA, GPIO.HIGH)
-		else:
-			GPIO.output(PWMA, GPIO.LOW)
-		print('A forward', motorShouldRun)
+    @cherrypy.expose
+    def SetMotorAforward(motorShouldRun):
+            if(motorShouldRun):
+                GPIO.output(AIN1, GPIO.HIGH)
+                GPIO.output(AIN2, GPIO.LOW)
+                GPIO.output(PWMA, GPIO.HIGH)
+            else:
+                GPIO.output(PWMA, GPIO.LOW)
+                print('A forward', motorShouldRun)
 
-	@cherrypy.expose
-	def SetMotorBforward(motorShouldRun):
-		if(motorShouldRun):
-			GPIO.output(BIN1, GPIO.HIGH)
-			GPIO.output(BIN2, GPIO.LOW)
-			GPIO.output(PWMB, GPIO.HIGH)
-		else:
-			GPIO.output(PWMB, GPIO.LOW)
-		print('B forward', motorShouldRun)
+    @cherrypy.expose
+    def SetMotorBforward(motorShouldRun):
+        if(motorShouldRun):
+            GPIO.output(BIN1, GPIO.HIGH)
+            GPIO.output(BIN2, GPIO.LOW)
+            GPIO.output(PWMB, GPIO.HIGH)
+        else:
+            GPIO.output(PWMB, GPIO.LOW)
+            print('B forward', motorShouldRun)
 
-	@cherrypy.expose
-	def SetMotorAbackward(motorShouldRun):
-		if(motorShouldRun):
-			GPIO.output(AIN1, GPIO.LOW)
-			GPIO.output(AIN2, GPIO.HIGH)
-			GPIO.output(PWMA, GPIO.HIGH)
-		else:
-			GPIO.output(PWMA, GPIO.LOW)
-		print('A forward', motorShouldRun)
+    @cherrypy.expose
+    def SetMotorAbackward(motorShouldRun):
+        if(motorShouldRun):
+            GPIO.output(AIN1, GPIO.LOW)
+            GPIO.output(AIN2, GPIO.HIGH)
+            GPIO.output(PWMA, GPIO.HIGH)
+        else:
+            GPIO.output(PWMA, GPIO.LOW)
+            print('A forward', motorShouldRun)
 
-	@cherrypy.expose
-	def SetMotorBbackward(motorShouldRun):
-		if(motorShouldRun):
-			GPIO.output(BIN1, GPIO.LOW)
-			GPIO.output(BIN2, GPIO.HIGH)
-			GPIO.output(PWMB, GPIO.HIGH)
-		else:
-			GPIO.output(PWMB, GPIO.LOW)
-		print('B forward', motorShouldRun)
+    @cherrypy.expose
+    def SetMotorBbackward(motorShouldRun):
+        if(motorShouldRun):
+            GPIO.output(BIN1, GPIO.LOW)
+            GPIO.output(BIN2, GPIO.HIGH)
+            GPIO.output(PWMB, GPIO.HIGH)
+        else:
+            GPIO.output(PWMB, GPIO.LOW)
+            print('B forward', motorShouldRun)
 
-
-	@cherrypy.expose
-	def BothMotorsBackwards():
+    @cherrypy.expose
+    def BothMotorsBackwards():
         motorController.SetMotorAbackward(True)
         motorController.SetMotorBbackward(True)
         return index_html
 
-	@cherrypy.expose
-	def index(self):
-		return index_html
+    @cherrypy.expose
+    def index(self):
+        return index_html
 
-	@cherrypy.expose
-	def Forward(self):
-		print ('Set both motors to move forward')
-		motorController.SetMotorAforward(True)
-		motorController.SetMotorBforward(True)
-		return index_html
+    @cherrypy.expose
+    def Forward(self):
+        print ('Set both motors to move forward')
+        motorController.SetMotorAforward(True)
+        motorController.SetMotorBforward(True)
+        return index_html
 
-	@cherrypy.expose
-	def Stop(self):
-		print ('Set both motors to stop')
-		motorController.SetMotorAforward(False)
-		motorController.SetMotorBforward(False)
-		return index_html
+    @cherrypy.expose
+    def Stop(self):
+        print ('Set both motors to stop')
+        motorController.SetMotorAforward(False)
+        motorController.SetMotorBforward(False)
+        return index_html
 
-	@cherrypy.expose
-	def Right(self):
+    @cherrypy.expose
+    def Right(self):
         print ('Set 1 motor high, and 1 motor low')
         motorController.SetMotorAforward(True)
         motorController.SetMotorBbackward(True)
@@ -112,6 +109,7 @@ class motorController:
         motorController.SetMotorBbackward(True)
         motorController.SetMotorAbackward(True)
         return index_html
+
 
 cherrypy.server.socket_host = '0.0.0.0'
 cherrypy.quickstart(motorController())
