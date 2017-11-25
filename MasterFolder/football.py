d044e9e4cfcfc8281902b988593dd588e17d4692
rimantas <rimantas.smitas@gmail.com>
2017-11-25 11:37:34 +0100

    Wheel even

--- MasterFolder/football.py
+++ MasterFolder/football.py
@@ -29,7 +29,7 @@ GPIO.setup(BIN2, GPIO.OUT)
 
 defaultSpeed = 90
 defaultLSpeed = defaultSpeed
-defaultRSpeed = defaultSpeed - 2.6
+defaultRSpeed = defaultSpeed - 2.4
 turnSpeedDifferenceLight = 40
 turnSpeedDifferenceHard = 25
 spinSpeedMultiplication = 0.25
