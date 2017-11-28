e59fc30a059abddc4e93a94227e4f88dff626a33
MonikaSty <mostyx@gmail.com>
2017-11-28 12:00:40 +0100

    koko

--- MasterFolder/LineFollow5.py
+++ MasterFolder/LineFollow5.py
@@ -11,8 +11,8 @@ sleeptimer = 0.005
 defaultSpeed = 70
 defaultLSpeed = defaultSpeed
 defaultRSpeed = defaultSpeed - 2.4
-turnSpeedDifferenceLight = 45
-spinSpeedMultiplication = 0.25
+turnSpeedDifferenceLight = 40
+spinSpeedMultiplication = 0.30
 
 
 
