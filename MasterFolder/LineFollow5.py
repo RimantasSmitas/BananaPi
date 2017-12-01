c37cd64a50ce8a7ae68287f2f03ffc327c6940a1
rimantas <rimantas.smitas@gmail.com>
2017-12-01 10:18:33 +0100

    LineFollow

--- MasterFolder/LineFollow5.py
+++ MasterFolder/LineFollow5.py
@@ -11,8 +11,8 @@ sleeptimer = 0.005
 defaultSpeed = 65
 defaultLSpeed = defaultSpeed
 defaultRSpeed = defaultSpeed - 2.4
-turnSpeedDifferenceLight = 35
-spinSpeedMultiplication = 0.30
+turnSpeedDifferenceLight = 30
+spinSpeedMultiplication = 0.25
 
 
 def lineFollow():
